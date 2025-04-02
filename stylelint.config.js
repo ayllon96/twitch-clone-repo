4a80a716363f5bbf0152a7d7ce92335fa5e9240d
Daniel <johnmejillon96@gmail.com>
2025-04-02 13:17:13 +0200

    test: testing pre-commit hook

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-vue'
+  ],
+  rules: {
+    'selector-class-pattern': null,
+    'value-keyword-case': null
+  }
+}
